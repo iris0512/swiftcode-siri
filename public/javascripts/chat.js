3d5996b8447b2eb0ec832474c474637ea444f854
iris0512 <mrudula0512@gmail.com>
2018-04-14 12:13:52 +0000

    Message Display

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,5 +1,23 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope) {
+    $scope.messages = [
+        {
+            'sender': 'USER',
+            'text': 'Hello'
+				},
+        {
+            'sender': 'BOT',
+            'text': 'What can I do for you?'
+				},
+        {
+            'sender': 'USER',
+            'text': 'Help me search'
+				},
+        {
+            'sender': 'BOT',
+            'text': 'What would you like me to search for?'
+				}
+			];
 
 });
